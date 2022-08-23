49144bc27b4ea77210ee9595042c382ade76d134
aysenurgozturk <aysenurgozturkk@gmail.com>
2022-08-23 16:08:35 +0300

    commit

---
+++ app/src/main/java/com/example/earthquakewatcherapp/UI/CustomInfoWindow.java
@@ -0,0 +1,45 @@
+package com.example.earthquakewatcherapp.UI;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.earthquakewatcherapp.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+import org.w3c.dom.Text;
+
+public class CustomInfoWindow implements GoogleMap.InfoWindowAdapter {
+    private View view;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public CustomInfoWindow(Context context){
+        this.context= context;
+
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = layoutInflater.inflate(R.layout.custom_info_window, null);
+    }
+    @Nullable
+    @Override
+    public View getInfoContents(@NonNull Marker marker) {
+
+        TextView title = (TextView) view.findViewById(R.id.winTitle);
+        title.setText(marker.getTitle());
+
+        TextView magnitude = (TextView) view.findViewById(R.id.magnitude);
+        magnitude.setText(marker.getSnippet());
+        return view;
+    }
+
+    @Nullable
+    @Override
+    public View getInfoWindow(@NonNull Marker marker) {
+        return null;
+    }
+}
