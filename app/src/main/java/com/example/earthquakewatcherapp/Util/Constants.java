558d77ea8305e6b531d8fcc67e487544a37279ee
aysenurgozturk <aysenurgozturkk@gmail.com>
2022-08-23 16:10:10 +0300

    commit

--- app/src/main/java/com/example/earthquakewatcherapp/Util/Constants.java
+++ app/src/main/java/com/example/earthquakewatcherapp/Util/Constants.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class Constants {
     public static final String URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
     public static final int LIMIT = 50;
-    public static final String YOUR_API_KEY = "AIzaSyA62vVhqtC-bVCyLBe6s-8fQ3TAbQaorBw";
 
     public static int randomInt(int max, int min) {
         return new Random().nextInt(max - min) + min;
